fd3c9ba73861d2271637798b1583d32cd54a2ee0
Arun Bisht <arun.bisht@nagarro.com>
2016-12-19 11:27:34 +0530

    Linking created

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -42,7 +42,7 @@ module.exports = function(config) {
     ],
 
     // web server port
-    port: 8080,
+    port: 2323,
 
     // Start these browsers, currently available:
     // - Chrome
---
+++ test/spec/controllers/home.controller.js
@@ -0,0 +1,3 @@
+/**
+ * Created by arunbisht on 19-12-2016.
+ */
