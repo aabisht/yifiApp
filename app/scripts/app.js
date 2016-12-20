87addae91fb58fb54547061adadd72e1a4ec1da5
Arun Bisht <arun.bisht@nagarro.com>
2016-12-20 12:37:56 +0530

    View IMDB Data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,7 +39,8 @@
             controller: 'MovieDetailController',
             controllerAs: 'MovieDetailCtrl',
             params: {
-              movieID: ''
+              movieID: '',
+              imdbCode: ''
             }
           });
           $urlRouterProvider.otherwise('home');
--- app/scripts/controllers/movie.detail.controller.js
+++ app/scripts/controllers/movie.detail.controller.js
@@ -19,9 +19,10 @@
 
         self.movieDetails = {
           movieId: $stateParams.movieID,
+          imdb_code: $stateParams.imdbCode,
           movieData: [],
           movieSuggestion: [],
-          movieComments: [],
+          movieIMDB_Data: [],
           sliderConfiguration: []
         };
 
@@ -42,7 +43,6 @@
           }
 
           var dataURL = CONSTANTS.BASE_API_URL + "movie_details.json?movie_id="+self.movieDetails.movieId;
-          console.log(dataURL);
           dataFactory.getData(dataURL)
             .then(function (responseData) {
               self.movieDetails.movieData = responseData.data.movie;
@@ -56,14 +56,14 @@
             }, function (error) {
               error.log(error)
           });
-          // dataURL = CONSTANTS.BASE_API_URL + "movie_comments.json?movie_id="+self.movieDetails.movieId;
-          // dataFactory.getData(dataURL)
-          //   .then(function (responseData) {
-          //     self.movieDetails.movieComments = responseData.data;
-          //   }, function (error) {
-          //     error.log(error)
-          // });
-          console.log(self.movieDetails);
+          var imdbDataURL = "https://testnode25.herokuapp.com/" + self.movieDetails.imdb_code;
+          dataFactory.getData(imdbDataURL)
+            .then(function (responseIMDBData) {
+              self.movieDetails.movieIMDB_Data = responseIMDBData.imdbData;
+              console.log(self.movieDetails);
+            }, function (error) {
+              error.log(error);
+          })
         }
 
       }]);
